535c3c54a97e482fa5c941f9a55539584217e2c6
Leonardo Carrere <leocarrerecm@gmail.com>
2023-01-27 09:06:38 -0300

    added delete, put and get routes

--- src/main/java/com/pokemon/pokemon/controller/PokemonController.java
+++ src/main/java/com/pokemon/pokemon/controller/PokemonController.java
@@ -3,10 +3,8 @@ package com.pokemon.pokemon.controller;
 
 import com.pokemon.pokemon.domain.Pokemon;
 import com.pokemon.pokemon.service.PokemonService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -26,15 +24,27 @@ public class PokemonController {
     }
 
 
+    @GetMapping("/v1/pokemon/{id}")
+    public Pokemon getPokemonById(@PathVariable Long id) {
+        return service.getPokemonById(id);
+    }
+
     @PostMapping("/v1/pokemon")
     public Pokemon newPokemon(@RequestBody Pokemon newPokemon) {
         return service.create(newPokemon);
     }
 
 
+    @PutMapping("/v1/pokemon")
+    public Pokemon updatePokemon(@RequestBody Pokemon updatedPokemon, @PathVariable Long id) {
+        return service.replacePokemonById(id, updatedPokemon);
+    }
 
 
-
+    @DeleteMapping("/v1/pokemon")
+    public void deletePokemon(@PathVariable Long id) {
+        service.deletePokemonById(id);
+    }
 
 
 }
